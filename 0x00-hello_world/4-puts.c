1c78988746b16eb1d3cd9e743d8041bf9631edf1
sam-i-e <samierapo@outlook.com>
2022-09-08 10:06:57 -0700

    "commit"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 /**
-   * main - Entry point
-
-    *
- 
-     * RETURN: Always 0 (Success)
- 
-      */
+ * main - Entry point
+ *
+ * RETURN: Always 0 (Success)
+ */
 
 int main(void)
 
