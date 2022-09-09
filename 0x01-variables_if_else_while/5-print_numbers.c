b8233fe3806645556702890e97a1b772c83b0396
sam-i-e <samierapo@outlook.com>
2022-09-09 09:20:03 -0700

    "commit"

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - Prints the numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
