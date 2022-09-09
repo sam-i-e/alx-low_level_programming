9afc3ca8ad8835a9b1ecfde42f06164a016e310a
sam-i-e <samierapo@outlook.com>
2022-09-09 10:52:21 -0700

    "commit"

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints all possible combinations of two different digits, in ascending
+ * main - Prints all possible combinations of two different digits
  *
  * Return: Always 0 (Success)
  */
