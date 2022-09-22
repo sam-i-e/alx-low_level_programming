7a919cbe8416126fdccf839254a3a1dee172c0a7
sam-i-e <samierapo@outlook.com>
2022-09-22 02:13:01 -0700

    "commit"

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,9 +9,9 @@ void reverse_array(int *a, int n)
 {
 	int tmp, i;
 
-	for (i = n -1; i >= n /2; i--)
+	for (i = n - 1; i >= n / 2; i--)
 	{
-		tmp = a[n -1 -i];
+		tmp = a[n - 1 - i];
 		a[n - 1 - i] = a[i];
 		a[i] = tmp;
 	}
