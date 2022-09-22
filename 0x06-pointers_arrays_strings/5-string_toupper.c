da15375ad42fbcbce2853bf1e26cca5859518aba
sam-i-e <samierapo@outlook.com>
2022-09-22 03:31:54 -0700

    "commit"

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,14 +7,14 @@
  *
  * Return: A pointer to the changed string.
  */
-char *string_troupper(char *str)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
 	while (str[i])
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
+			str[i] -= 32;
 
 		i++;
 	}
