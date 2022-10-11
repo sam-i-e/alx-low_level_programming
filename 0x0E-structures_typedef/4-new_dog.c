e5dd4fc4ad330b38fcb85e6b75894bc904f26fe6
sam-i-e <samierapo@outlook.com>
2022-10-12 00:33:00 +0300

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,6 +22,25 @@ int _strlen(char *s)
  * @src: origin of the string
  * Return: dest destine copie
  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner of the dog
+ * Return: dog created
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
